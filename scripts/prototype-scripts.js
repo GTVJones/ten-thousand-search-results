c98d29477360ca2d91eb108acf2016233fcae15b
Gwyn Jones <m@gwnjns.com>
2015-07-28 13:45:27 +0100

    Adding ability to differentiate between users who are/not signed in

--- scripts/prototype-scripts.js
+++ scripts/prototype-scripts.js
@@ -4,8 +4,13 @@ app.controller('downloadCtrl', function ($scope) {
     $scope.data = {
         formVisibility : false,
         numberOfResults : 6072107,
-        maxDownloadable : 10000
+        maxDownloadable : 1000,
+        signedIn : false
     }
+
+    $scope.$watch('data.signedIn', function(newValue) {
+        $scope.data.maxDownloadable = (newValue === true) ? 10000 : 1000;
+    })
 
     $scope.proportionOfResults = function() {
         var percentage = ($scope.data.maxDownloadable / $scope.data.numberOfResults) * 100;
